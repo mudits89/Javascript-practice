d2a28465ec76ae95a39c5a55b980c8c9b0d541e4
mudits89 <mudits_89@hotmail.com>
2018-04-04 13:32:01 +0800

    Add files via upload

---
+++ intro2 - JS.js
@@ -0,0 +1,57 @@
+// This line of code sets the variable location to the string New York City
+const location = 'New York City';
+
+// This line of code sets the variable latitude to the number 40.7
+let latitude = 40.7;
+
+// This line of code sets the variable inNorthernHemisphere to true
+let inNorthernHemisphere = true;
+celsius = 15;
+console.log(celsius);
+
+Fahrenheit = celsius * (9/5) + 32;
+console.log(Fahrenheit);
+console.log(location);
+console.log(latitude);
+console.log(inNorthernHemisphere);
+
+
+const entree = 'Enchiladas';
+console.log(entree);
+const price = 12;
+console.log(price);
+entree = 'Tacos';
+
+
+let changeMe = true;
+console.log(changeMe);
+changeMe = false;
+console.log(changeMe);
+
+
+let notDefined;
+let valueless;
+
+
+let molecule = 16;
+let particle = 18;
+let assay = 3;
+
+// Add and assign to molecule below
+molecule += 16;
+
+// Multiply and assign to particle below
+
+particle *= 6.02;
+// Increment assay by 1
+assay += 1;
+
+
+let favoriteAnimal = 'Elephant';
+console.log('My favorite animal is ' + favoriteAnimal + '.')
+
+let myName = 'Mudit';
+let myCity = 'Singapore';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+
+
