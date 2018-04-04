8e5591caffe829c3dcde04033030c6f03615bc32
mudits89 <mudits_89@hotmail.com>
2018-04-04 13:41:27 +0800

    Add files via upload

---
+++ prac1 -JS.js
@@ -0,0 +1,70 @@
+let userName = 'Mudit';
+let knowsJavaScript = true;
+
+if (knowsJavaScript && userName) {
+  console.log('Great, ' + userName + '! Get ready to practice your JavaScript!');
+} else if (knowsJavaScript) {
+  console.log('Great! Get ready to practice your JavaScript!');
+} else if (userName) {
+  console.log('Great, ' + userName + '! Get ready to learn something new!');
+} else {
+  console.log('Great! Get ready to learn something new!');
+}
+
+
+let isSoccerFan = true;
+if (isSoccerFan === true) {
+  console.log('Goal!')
+} else{
+  console.log( 'No Goal!')
+}
+
+
+let wordCount = 16;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '1';
+
+if (!favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+
+let wordCount = 16;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '1';
+
+if (!favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+
+let hungerLevel = 10;
+
+if (hungerLevel > 7) {
+  console.log('Time to eat!')
+} else {
+  console.log('We can eat later!')
+}
+
+
+
+
+
