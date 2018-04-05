09c46cb30dae86ad619bc12bb55424df5435a7f5
mudits89 <mudits_89@hotmail.com>
2018-04-05 10:11:30 +0800

    Add files via upload

---
+++ grocerylist.js
@@ -0,0 +1,71 @@
+//setting an array and printing the same
+let newYearsResolutions = ['my name is Mudit', 'I am the best', 'please take care'];
+
+console.log(newYearsResolutions);
+
+//printing from array using different places starting from index based at 0.
+let newYearsResolutions = ['my name is Mudit', 'I am the best', 'please take care'];
+
+console.log(newYearsResolutions);
+
+let listItem = (newYearsResolutions[0]);
+console.log(listItem);
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);
+
+
+//replacing the second element in the array
+let newYearsResolutions = ['my name is Mudit', 'I am the best', 'please take care'];
+
+console.log(newYearsResolutions);
+
+let listItem = (newYearsResolutions[0]);
+console.log(listItem);
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);
+newYearsResolutions[1] = 'Learn a new language';
+console.log(newYearsResolutions);
+
+
+//to find out length of the array
+let newYearsResolutions = ['my name is Mudit', 'I am the best', 'please take care'];
+
+console.log(newYearsResolutions);
+
+let listItem = (newYearsResolutions[0]);
+console.log(listItem);
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);
+newYearsResolutions[1] = 'Learn a new language';
+console.log(newYearsResolutions);
+console.log(newYearsResolutions.length);
+
+
+// push (add) and pop (remove) last item in the array
+let newYearsResolutions = ['my name is Mudit', 'I am the best', 'please take care'];
+
+console.log(newYearsResolutions);
+
+let listItem = (newYearsResolutions[0]);
+console.log(listItem);
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);
+newYearsResolutions[1] = 'Learn a new language';
+console.log(newYearsResolutions);
+console.log(newYearsResolutions.length);
+newYearsResolutions.push('true faces are visible later', 'I am not perfect');
+console.log(newYearsResolutions);
+newYearsResolutions.pop();
+console.log(newYearsResolutions);
+
+//using slice function to select between two index numbers items that are present
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+
+groceryList.unshift('popcorn');
+//console.log(groceryList);
+
+console.log(groceryList.slice(1, 4));
+console.log(groceryList);
+
