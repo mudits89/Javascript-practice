5ae3f2944b2046e49489edb7c08848a5b20f3284
mudits89 <mudits_89@hotmail.com>
2018-04-18 15:32:58 +0800

    Add files via upload

---
+++ Objects - 3.js
@@ -0,0 +1,67 @@
+//setters with if condition
+let person = {
+  _name: 'Lu Xun',
+  _age: 137,
+  
+  set age(ageIn) {
+    if (typeof ageIn === 'number') {
+      this._age = ageIn;
+    }
+    else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    }
+  }
+
+};
+
+//set age in string and number
+let person = {
+  _name: 'Lu Xun',
+  _age: 137,
+  
+  set age(ageIn) {
+    if (typeof ageIn === 'number') {
+      this._age = ageIn;
+    }
+    else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    }
+  }
+	set person.age = 'Thirty-nine';
+  set person.age = 39;
+};
+
+
+//print the age of the person
+let person = {
+  _name: 'Lu Xun',
+  _age: 137,
+  
+  set age(ageIn) {
+    if (typeof ageIn === 'number') {
+      this._age = ageIn;
+    }
+    else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    }
+  },
+  
+  get age() {
+    console.log(`${this._name} is ${this._age} years old.`);
+    return this._age;
+  }
+
+};
+
+
+person.age = 'Thirty-nine';
+person.age = 39;
+
+console.log(person.age);
+
+
+
+
