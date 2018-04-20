59d4665b1d1561c504633c58f99ccaa49ab56ae5
mudits89 <mudits_89@hotmail.com>
2018-04-20 11:20:11 +0800

    Add files via upload

---
+++ npm install babel es5 and es6.js
@@ -0,0 +1,29 @@
+// Set the variable below to a number
+let esFivePercentageSupport = 97.81;
+
+// Set the variable below to a number
+let esSixTemplateLiterals = 7.67;
+
+
+var pasta = "Spaghetti"; // ES5 syntax
+
+var meat = "Pancetta"; // ES5 syntax
+
+var sauce = "Eggs and cheese"; // ES5 syntax
+
+// ES5 syntax
+var carbonara = 'You can make carbonara with' + pasta + ','  + meat + ', and a sauce made with' +sauce +'.';
+
+
+npm install babel-cli
+npm install babel-preset-env
+npm run build
+
+//to see source file
+ls 
+//to create package.json file
+npm init
+
+//with devdependencies
+$ npm install babel-cli -D
+$ npm install babel-preset-env -D
