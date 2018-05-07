7cf942383fde69d8c90fcf15c77f6a860c65c413
mudits89 <mudits_89@hotmail.com>
2018-05-07 19:16:35 +0800

    Add files via upload

---
+++ Ajax request with $.js
@@ -0,0 +1,121 @@
+//AJAX requests with $.get()
+// Include data for accessing Google APIs
+
+const apiKey = '<Your API Key>';
+const url = 'https://www.googleapis.com/urlshortener/v1/url';
+
+// Some page elements
+
+const $inputField = $('#input');
+const $expandButton = $('#expand');
+const $shortenButton = $('#shorten');
+const $responseField = $('#responseField');
+
+// AJAX functions
+
+function expandUrl() {
+  const urlToExpand = url + 
+        '?key=' + apiKey +
+        '&shortUrl=' + $inputField.val();
+  $.get(urlToExpand, response => {
+  	$responseField.append('<p>Your expanded url is: </p><p>' +
+  	response.longUrl + '</p>');
+	}, 'json');
+}
+
+function shortenUrl() {
+  const urlWithKey = url + '?key=' + apiKey;
+  const urlToShorten = $inputField.val();
+
+  $.ajax({
+    url: urlWithKey, 
+    type: 'POST', 
+    data: JSON.stringify({longUrl: urlToShorten}), 
+    dataType: 'json', 
+    contentType: 'application/json', 
+    success(response) {
+      $responseField.append('<p>Your shortened url is: </p><p>' + response.id + '</p>');
+    },
+    error(jqXHR, status, errorThrown) {
+      console.log(jqXHR);
+    }
+  });
+}
+
+function expand() {
+  $responseField.empty();
+  expandUrl();
+  return false;
+}
+
+function shorten() {
+  $responseField.empty();
+  shortenUrl();
+  return false;
+}
+
+// Call functions on submit
+
+$expandButton.click(expand);
+$shortenButton.click(shorten);
+
+
+//AJAX requests with $.post()
+// Include data for accessing Google APIs
+
+const apiKey = '<Your API Key>';
+const url = 'https://www.googleapis.com/urlshortener/v1/url';
+
+// Some page elements
+
+const $inputField = $('#input');
+const $expandButton = $('#expand');
+const $shortenButton = $('#shorten');
+const $responseField = $('#responseField');
+
+// AJAX functions
+
+function expandUrl() {
+  const urlToExpand = url + 
+        '?key=' + apiKey +
+        '&shortUrl=' + $inputField.val();
+  $.get(urlToExpand, response => {
+  	$responseField.append('<p>Your expanded url is: </p><p>' +
+  	response.longUrl + '</p>');
+	}, 'json');
+}
+
+function shortenUrl() {
+  const urlWithKey = url + '?key=' + apiKey;
+  const urlToShorten = $inputField.val();
+
+  $.post({
+    url: urlWithKey,  
+    data: JSON.stringify({longUrl: urlToShorten}), 
+    dataType: 'json', 
+    contentType: 'application/json', 
+    success(response) {
+      $responseField.append('<p>Your shortened url is: </p><p>' + response.id + '</p>');
+    },
+    error(jqXHR, status, errorThrown) {
+      console.log(jqXHR);
+    }
+  });
+}
+
+function expand() {
+  $responseField.empty();
+  expandUrl();
+  return false;
+}
+
+function shorten() {
+  $responseField.empty();
+  shortenUrl();
+  return false;
+}
+
+// Call functions on submit
+
+$expandButton.click(expand);
+$shortenButton.click(shorten);
